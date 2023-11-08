d1b3d0f51fb4cbc046700cef06414cd6262b835a
Albert Tazetdinov <zet565758@gmail.com>
2023-11-08 21:15:43 +0300

    исправил баг с отображением количества товаров

--- src/pages/Bin/CardProduct/CardProduct.jsx
+++ src/pages/Bin/CardProduct/CardProduct.jsx
@@ -1,30 +1,28 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import cl from "./CardProduct.module.css";
 import {ButtonDeleteProduct} from "./ButtonDeleteProduct/ButtonDeleteProduct";
 
-export const CardProduct = ({index, img, name, size, price, item, count, setBinData, binData}) => {
+export const CardProduct = ({index, img, name, size, price, item, count, setBinData, binData, setRender}) => {
 
     const [countProduct, setCountProduct] = useState(count)
 
     return (
-        <li key={index} className={cl.bin_item}>
+        <div key={index} className={cl.bin_item}>
             <div className={cl.mainInfo}>
                 <div className={cl.bin_item__left}>
                     <img src={img}/>
                     <div className={cl.bin__text}>
                         <p>{name}</p>
                         <p className={cl.blockSizes}>Размеры:
-                            <div className={cl.listSizes}>
+                            <span className={cl.listSizes}>
                                 {size.map(elem => (
                                     size.at(-1) === elem
                                         ?
                                         <span>{elem}</span>
                                         :
                                         <span>{elem},</span>
-
-
                                 ))}
-                            </div>
+                            </span>
                         </p>
                         <p>Количество: {countProduct}</p>
                     </div>
@@ -36,8 +34,9 @@ export const CardProduct = ({index, img, name, size, price, item, count, setBinD
                 setBinData={setBinData}
                 binData={binData}
                 setCountProduct={setCountProduct}
+                countProduct={countProduct}
             />
-        </li>
+        </div>
     );
 };
 
